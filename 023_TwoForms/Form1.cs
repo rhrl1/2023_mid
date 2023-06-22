9efb765d48f9953334455eb0598226d9be273284
rhrl1 <127263684+rhrl1@users.noreply.github.com>
2023-06-22 13:33:31 +0900

    Add files via upload

---
+++ 023_TwoForms/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _023_TwoForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form2 f;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(f == null)  // Form2가 없을 때만 만든다
+             f = new Form2(this,"난 Form1이야.");
+            f.Show();
+            this.Hide(); //Form1 없어짐
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = f.textBox1.Text;
+        }
+
+        // Common 클래스에서 값을 가져오기
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Common.str + "\n" + Common.value);
+        }
+    }
+
+    public static class Common
+    {
+        // 멤버 변수 = field(필드) dataBase 처럼 모두 쓸 수 있는 공통의 클래스
+        public static string str = "";
+        public static int value = 0;
+    }
+}
---
+++ 023_TwoForms/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _023_TwoForms
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Form1 form, string s)
+        {
+            InitializeComponent();
+            f = form;
+            MessageBox.Show(s, "이곳은 Form2");
+        }
+
+        Form1 f;
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            f.Show();
+        }
+
+        // Form2에서 Form1의 Text 변경( 제목 )
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            f.Text = textBox1.Text;
+
+        }
+
+        // Form2에서 Form1의 label1 변경
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Fomr1의 label1의 Modifiers 속성을 public으로 바꾼 다음
+            f.label1.Text = textBox1.Text;
+        }
+
+        // Form2 에서 Common 클래스의 값을 지정
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Common.str = textBox1.Text;
+            Common.value = 1024;
+        }
+    }
+}
